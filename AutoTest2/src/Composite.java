5c96b4e31a466b8e10e5e277001ee0a3df604c85
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2023-12-27 13:11:58 +0300

    Add files via upload

---
+++ AutoTest2/src/Composite.java
@@ -0,0 +1,28 @@
+public class Composite implements Products {
+
+    private List<Products> products = new ArrayList<>();
+
+    void addProduct(Products product) {
+        products.add(product);
+    }
+
+    void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    @Override
+    public void writeDown() {
+        for (Products product : products) {
+            product.writeDown();
+        }
+    }
+
+    @Override
+    public Long getSum() {
+        AtomicReference<Long> result = new AtomicReference<>(0L);
+        products.forEach(iProduct -> {
+            result.set(result.get() + iProduct.getSum());
+        });
+        return result.get();
+    }
+}
